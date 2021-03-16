13af76793fae076e10471e47801bfb14fdf67361
vincecachi <vincentcachila@gmail.com>
2021-03-16 09:53:06 +0800

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_NAME', 'rmd_db');
+define('DB_USERNAME', 'u683594569_unixondev');
+define('DB_PASSWORD', 'Unixondev2021!');
+define('DB_NAME', 'u683594569_rmd_db');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME) or die("Something Went Wrong" . mysqli_connect_error());
