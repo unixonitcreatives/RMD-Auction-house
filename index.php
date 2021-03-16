02f5d63b90ca28f02433b54c1a00df0f4e47e4f4
vincecachi <vincentcachila@gmail.com>
2021-03-16 15:04:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ require_once "config.php";
 
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] == false){
+  echo "<script>alert('Hello! I am an alert box!');</script>";
   header("location: login.php");
   exit;
 }
